0e976b7592296870f2a2eff3df0219cfbbdaa39b
kolluru sridhar <87171968+sridharshree303@users.noreply.github.com>
2022-12-28 15:11:34 +0530

    c

--- ProblemSolving/src/assign/PeopleInCircle.java
+++ ProblemSolving/src/assign/PeopleInCircle.java
@@ -13,35 +13,6 @@ public class PeopleInCircle {
 		for (int i = 0; i < n; i++) {
 			 System.out.print(li.get(i)+" ");
 		}
-		System.out.println();
-		int size = li.size();
-		int rem = 1;
-		while(size > 2) {
-			//removed element is last then rem = 1
-			//else rem != last[ele]; rem = 0;
-			
-			int plast = li.get(size-1);
-			int i = 0;
-			while (i < size / 2) {
-				li.remove(i + rem);
-				i++;
-				
-			}
-			System.out.println(li);
-			size = li.size();
-//			System.out.println(size);
-			int nlast = li.get(size-1);
-//			System.out.println(nlast+" - "+plast+" - "+rem);
-			if(nlast != plast) {
-				rem = 1;
-				li.remove(size-1);
-			}else {
-				rem = 0;
-			}
-			
-		}
-		System.out.println(li.get(0));
-	
 	}
 }
 
