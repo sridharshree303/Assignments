25bca6b462e7ec858566cf6eb8be47398bc97336
kolluru sridhar <87171968+sridharshree303@users.noreply.github.com>
2023-01-28 12:52:21 +0530

    Update ReverseLinkedList.java

--- ProblemSolving/src/tt10_linkedlist/ReverseLinkedList.java
+++ ProblemSolving/src/tt10_linkedlist/ReverseLinkedList.java
@@ -1,5 +1,30 @@
 package tt10_linkedlist;
 
 public class ReverseLinkedList {
-
+	public static void main(String[] args) {
+		ListNode A = new ListNode(1);
+		ListNode B = new ListNode(2);
+		ListNode C = new ListNode(3);
+		ListNode D = new ListNode(4);
+		
+		A.next = B;
+		B.next = C;
+		C.next = D;
+		
+		solve(A);
+	}
+	
+	//by using recursion
+	 public static void solve(ListNode A) {
+		  printNodes(A);
+	 }
+	 
+	 public static void printNodes(ListNode A) {
+		//base condition
+		  if(A == null)
+			  return ;
+		//main Logic
+		 printNodes(A.next);
+		 System.out.print(A.val+" ");
+	 }
 }
